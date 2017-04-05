3a520a52074d67cc3c12f7cdc3a3ada7936430b5
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-05 15:10:25 -0700

    prints out first title

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,19 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).on("ready", function() {
+   $.ajax( {
+    method: "GET",
+    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson" , 
+    dataType: "json",
+    success: onSuccess
+  });   // ajax
+});  // doc on ready
+  function onSuccess(responseData) {
+    console.log(responseData);
 
-  // CODE IN HERE!
-
-});
+    var output = responseData.features[0].properties.title;
+    // [0].properties.title;
+    // for each (var in output) {
+    $("#info").append('<p>' + output+ '</p>');
+  
+}; 
