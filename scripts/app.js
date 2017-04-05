1ee41b3600ed136128e9a939aa7ee4c586338ed5
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-05 15:15:39 -0700

    prints out all titles

--- scripts/app.js
+++ scripts/app.js
@@ -12,9 +12,13 @@ $(document).on("ready", function() {
   function onSuccess(responseData) {
     console.log(responseData);
 
-    var output = responseData.features[0].properties.title;
+    var output = responseData.features;
+    output.forEach(function(item, index) {
+    	var newOutput = output[index].properties.title;
+    	$("#info").append('<p>' + newOutput + '</p>');
+    })
     // [0].properties.title;
     // for each (var in output) {
-    $("#info").append('<p>' + output+ '</p>');
+   
   
 }; 
